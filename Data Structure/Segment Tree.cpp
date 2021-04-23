1e2e717834133835bcb3d3fe273508bcc3ca1cec
sam571128 <samlee.571128@gmail.com>
2021-04-23 13:13:16 +0800

    Modified Segment Tree

--- Data Structure/Segment Tree.cpp
+++ Data Structure/Segment Tree.cpp
@@ -1,57 +1,57 @@
 struct SegT{
-	int d[4*N];
-	int lazy_tag[4*N];
+	const int MAXN = 1e5+5;
+	int tr[MAXN*4], arr[MAXN], tag[MAXN*4]; 
+
 	int combine(int a, int b){
-		return a+b;
+	    return max(a,b);
 	}
-	void build(int a[], int ind = 1, int l = 0, int r = N-1){
-		if(l==r){
-			d[ind]=a[l];
-		}else{
-			int mid = (l+r)/2;
-			build(a,ind<<1,l,mid);
-			build(a,ind<<1|1,mid+1,r);
-			d[ind] = combine(d[ind<<1],d[ind<<1|1]);
-		}
+
+	void build(int idx, int l, int r){
+	    if(l==r){
+	        tr[idx] = arr[l]; 
+	    }else{
+	        int m = (l+r)/2;
+	        build(idx*2,l,m); 
+	        build(idx*2+1,m+1,r); 
+	        tr[idx] = combine(tr[idx*2],tr[idx*2+1]); 
+	    }
 	}
-	void modify(int pos, int val, int ind = 1, int l = 0, int r = N-1){
-		if(l==r){
-			d[ind] = val;
-		}else{
-			int mid = (l+r)/2;
-			if(pos<=mid) modify(pos,val,ind<<1,l,mid);
-			else modify(pos,val,ind<<1|1,mid+1,r);
-			d[ind] = combine(d[ind<<1],d[ind<<1|1]);
-		}
+
+	void push(int idx){
+	    if(tag[idx]){
+	        tr[idx<<1] = max(tr[idx<<1], tag[idx]);
+	        tr[idx<<1|1] = max(tr[idx<<1|1], tag[idx]);
+	        tag[idx<<1] = max(tag[idx<<1], tag[idx]);
+	        tag[idx<<1|1] = max(tag[idx<<1|1], tag[idx]);
+	        tag[idx] = 0;
+	    }
 	}
-	void range_modify(int ml, int mr, int val, int ind = 1, int l = 0, int r = N-1){
-		if(ml>r||mr<l) return;
-		if(ml<=l&&mr>=r){
-			lazy_tag[ind] += val;
-			d[ind] += (r-l+1)*val;
-			return;
-		}
-		int mid = (l+r)/2;
-		range_modify(ml,mr,val,ind<<1,l,mid);
-		range_modify(ml,mr,val,ind<<1|1,mid+1,r);
-		d[ind] = combine(d[ind<<1],d[ind<<1|1]);
+
+	void modify(int ql, int qr, int val, int idx, int l, int r){
+	    if(l!=r) push(idx); //當節點並非葉節點時，下推標記
+	    if(ql <= l && r <= qr){
+	        tr[idx] = max(tr[idx],val);
+	        tag[idx] = max(tag[idx],val);
+	        return;
+	    }
+	    int m = (l+r)/2;
+	    if(qr > m) modify(ql, qr, val, idx*2+1, m+1, r);
+	    if(ql <= m) modify(ql, qr, val, idx*2, l, m);
+	    tr[idx] = combine(tr[idx<<1],tr[idx<<1|1]);
 	}
-	void apply(int ind, int val, int l, int r){
-		if(ind>=0&&ind<4*N){
-			d[ind] += (r-l+1)*val;
-			lazy_tag[ind] += val;
-		}
+
+	int query(int ql, int qr, int idx, int l, int r){
+	    if(l!=r) push(idx); 
+	    if(ql <= l && r <= qr){
+	        return tr[idx];
+	    }
+	    int m = (l+r)/2;
+	    if(ql > m){
+	        return query(ql, qr, idx*2+1, m+1, r);
+	    }
+	    if(qr <= m){
+	        return query(ql, qr, idx*2, l, m);
+	    }
+	    return combine(query(ql, qr, idx*2, l, m), query(ql, qr, idx*2+1, m+1, r));
 	}
-	int query(int ql, int qr, int ind = 1, int l = 0, int r = N-1){
-		if(ql>r||qr<l) return 0;
-		if(ql<=l&&qr>=r) return d[ind];
-		int mid = (l+r)/2;
-		if(lazy_tag[ind]){
-			apply(ind<<1, lazy_tag[ind], l, mid);
-			apply(ind<<1|1, lazy_tag[ind], mid+1, r);
-			d[ind] = combine(d[ind<<1],d[ind<<1|1]);
-			lazy_tag[ind] = 0;
-		}
-		return combine(query(ql,qr,ind<<1,l,mid),query(ql,qr,ind<<1|1,mid+1,r));
-	}
-};
+}
