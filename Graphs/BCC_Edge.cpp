940d18befe92fa0f6dd6e7a09b78218a59048b5e
Cheng-Yuan Lee <53968281+sam571128@users.noreply.github.com>
2023-04-30 17:02:07 -0400

    Update BCC_Edge.cpp

--- Graphs/BCC_Edge.cpp
+++ Graphs/BCC_Edge.cpp
@@ -1,21 +1,21 @@
-void dfs(int u, int a){
-	d[u] = l[u] = t++;
+void dfs(int u, int p){
+	dfn[u] = low[u] = ++t;
 	st.emplace(u);
 	for(int v : adj[u]){
-		if(v == a) continue;
-		if(!d[v]){
+		if(v == p) continue;
+		if(!dfn[v]){
 			dfs(v,u);
-			//if(d[u] < l[v]) bridges.emplace_back(u,v);
-			l[u] = min(l[u],l[v]);
+			low[u] = min(low[u],low[v]);
 		}
-		l[u] = min(l[u],d[v]);
+		low[u] = min(low[u],dfn[v]);
 	}
-	if(l[u]==d[u]){
+	if(low[u]==dfn[u]){
 		bccid++;
 		int tmp;
 		do{
 			tmp = st.top(); st.pop();
 			bcc[tmp] = bccid;
+            sz[bccid]++;
 		}while(tmp!=u);
 	}
 }
